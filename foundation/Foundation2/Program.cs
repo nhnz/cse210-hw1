91afd3df175f70852de8f716a8122b6c87de9c39
nhnz <nmwnzama@yahoo.com>
2024-10-14 10:28:45 +0200

    create ordering system

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -34,7 +35,7 @@ class Program
 //      +----------------------------------+
 //      |             Product              | Store product details and calculate total cost.
 //      +----------------------------------+
-//      | -Name: string                    | 
+//      | -name: string                    | 
 //      | -Product: string                 |
 //      | -Price: decimal                  |
 //      | -Quantity: int                   | 
@@ -55,5 +56,43 @@ class Program
 //      | -GetShippingLabel(): string      |
 //      +----------------------------------+
 
+        // Addresses
+        Address address1 = new Address("478 Pinedale St","New York","NY","USA");
+        Address address2 = new Address("72 Bedford Av","Durban","KZN","RSA");
+
+        //Customers
+        Customer customer1 = new Customer("Will Smith", address1);
+        Customer customer2 = new Customer("Malcom Louw",address2);
+
+        //Products
+        Product product1 = new Product("Tires", "T001", 200.00m, 4);
+        Product product2 = new Product("Brake Pads", "B001", 400.00m, 4);
+        Product product3 = new Product("Air Filter", "A001", 90.00m, 1);
+        Product product4 = new Product("Fuel Filter", "F001", 180.00m, 1);
+
+        //Orders
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product1);
+        order2.AddProduct(product3);
+        order2.AddProduct(product4);
+
+        //Display order details
+        DisplayOrderDetails(order1);
+        DisplayOrderDetails(order2);
+
+    }
+
+    static void DisplayOrderDetails(Order order)
+    {
+       // Console.WriteLine("Packing Label:");
+        Console.WriteLine(order.GetPackingLabel() + "\n");
+        //Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Price: {order.CalculateTotal():C}");
+        Console.WriteLine();
     }
 }
